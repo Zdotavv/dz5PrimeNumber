42b25ae9b5da2c8374636316c109a53f42967de5
Vladimir <zdotavv@ukr.net>
2022-03-17 15:00:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            boolean primeNumber = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    primeNumber = false;
+                    break;
+                }
+            }
+            if (primeNumber) {
+                System.out.println(i);
+            }
+        }
+    }
+}
