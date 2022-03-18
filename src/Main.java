73640f8cb0f64e10653c24a1a8afa384f34ff00c
Vladimir <zdotavv@ukr.net>
2022-03-18 11:22:23 +0200

    Change numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Простые числа в диапазоне от 2 до 200: ");
+        System.out.println("Простые числа в диапазоне от 2 до 100: ");
         for (int i = 2; i <= 200; i++) {
             boolean primeNumber = true;
 
